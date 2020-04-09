1a017fe7d7d3b22b688353a4db3fe7f79d47a03e
arham-anees <arhamanees10@gmail.com>
2020-04-09 12:09:09 +0500

    basic structure second time

--- BLL/cUser.cs
+++ BLL/cUser.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace BLL
 {
-    class cUser
+    public class cUser
     {
-    }
+		#region Properties
+		public Int32 Id { get; set; }
+		public String FirstName { get; set; }
+		public String MiddleName { get; set; }
+		public String LastName { get; set; }
+		public String Username { get; set; }
+		public String Password { get; set; }
+
+		#endregion
+
+		#region Methods
+
+
+
+		#endregion
+
+	}
 }
--- Repositories/cUserRepository.cs
+++ Repositories/cUserRepository.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BLL;
 
 namespace Repositories {
-	public class cUserRepository {
+	public class cUserRepository : cRepository<cUser> {
 		public void AuthenticateUser()
 		{
 
