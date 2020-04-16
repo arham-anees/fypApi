4807a599458d7db38d094348bc0731f0bc69b64d
ArhamAnees <arhamanees10@gmail.com>
2020-04-16 15:27:20 +0500

    gitingore problem fixed

---
+++ BLL/cUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    public class cUser
+    {
+		#region Properties
+		public Int32 Id { get; set; }
+		public String FirstName { get; set; }
+		public String MiddleName { get; set; }
+		public String LastName { get; set; }
+		public String Username { get; set; }
+		public String Password { get; set; }
+
+		#endregion
+
+		#region Methods
+
+
+
+		#endregion
+
+	}
+}
---
+++ Repositories/cUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLL;
+
+namespace Repositories {
+	public class cUserRepository : cRepository<cUser> {
+		public void AuthenticateUser()
+		{
+
+		}
+	}
+}
